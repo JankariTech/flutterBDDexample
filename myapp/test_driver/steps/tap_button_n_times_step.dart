bffa54a202158326619f630bddb06bcb335eef58
Artur Neumann <artur@jankaritech.com>
2020-05-28 12:34:31 +0545

    Given step should preset the counter

--- myapp/test_driver/steps/tap_button_n_times_step.dart
+++ myapp/test_driver/steps/tap_button_n_times_step.dart
@@ -8,6 +8,7 @@ class GivenCounterIsSetTo extends Given1WithWorld<String, FlutterWorld> {
 
   @override
   Future<void> executeStep(String expectedCounter) async {
+    await tapButton(world, timeout, "increment", int.parse(expectedCounter));
     final locator = find.byValueKey("counter");
     final actualCount = await FlutterDriverUtils.getText(world.driver, locator);
     expectMatch(actualCount, expectedCounter);
@@ -20,9 +21,13 @@ class TapButtonNTimesStep extends When2WithWorld<String, int, FlutterWorld> {
 
   @override
   Future<void> executeStep(String buttonKey, int amount) async {
-    final locator = find.byValueKey(buttonKey);
-    for (var i = 0; i < amount; i += 1) {
-      await FlutterDriverUtils.tap(world.driver, locator, timeout: timeout);
-    }
+    await tapButton(world, timeout, buttonKey, amount);
+  }
+}
+
+Future<void> tapButton(FlutterWorld world, Duration timeout, String buttonKey, int amount) async {
+  final locator = find.byValueKey(buttonKey);
+  for (var i = 0; i < amount; i += 1) {
+    await FlutterDriverUtils.tap(world.driver, locator, timeout: timeout);
   }
 }
